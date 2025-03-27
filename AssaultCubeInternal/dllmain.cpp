135ec934b6331cba8bbe7e6c9a6857b1daaeff39
Umut Arslan <89416489+uarslan-git@users.noreply.github.com>
2025-03-27 23:57:54 +0100

    add uninjection shortcut

--- AssaultCubeInternal/dllmain.cpp
+++ AssaultCubeInternal/dllmain.cpp
@@ -4,7 +4,7 @@
 #include "genCode.h"
 
 void hook() {
-
+    FreeLibraryAndExitThread(hModule, 0);
 }
 
 void console() {
@@ -16,12 +16,14 @@ void console() {
     while (true) {
         std::string input;
         std::cin >> input;
-        if (input == "exit") break;
+        if (GetAsyncKeyState(VK_END)) break;
         if (input == "up") localPlayerPtr->pos.x += 5;
         if (input == "down") localPlayerPtr->pos.x -= 5;
         if (input == "print") std::cout << "Local Player Ptr" << localPlayerPtr << std::endl;
     }
+    if (f) fclose(f);
     FreeConsole();
+    FreeLibraryAndExitThread(hModule, 0);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -32,8 +34,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)console, 0, 0, 0);
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)hook, 0, 0, 0);
+        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)console, 0, 0, 0));
+        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)hook, 0, 0, 0));
 
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
