60a8c50b40072db27800a1a259e882e39aee1e3d
Umut Arslan <89416489+uarslan-git@users.noreply.github.com>
2025-04-04 13:14:02 +0200

    wip

--- AssaultCubeInternal/dllmain.cpp
+++ AssaultCubeInternal/dllmain.cpp
@@ -11,15 +11,12 @@ void console() {
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
-    freopen_s(&f, "CONIN$", "r", stdin);
     std::cout << "Console Created" << std::endl;
     while (true) {
-        std::string input;
-        std::cin >> input;
         if (GetAsyncKeyState(VK_END)) break;
-        if (input == "up") localPlayerPtr->pos.x += 5;
-        if (input == "down") localPlayerPtr->pos.x -= 5;
-        if (input == "print") std::cout << "Local Player Ptr" << localPlayerPtr << std::endl;
+        if (GetAsyncKeyState(VK_UP)) localPlayerPtr->pos.x += 5;
+        if (GetAsyncKeyState(VK_DOWN)) localPlayerPtr->pos.x -= 5;
+        //if (input == "print") std::cout << "Local Player Ptr" << localPlayerPtr << std::endl;
     }
     if (f) fclose(f);
     FreeConsole();
@@ -34,9 +31,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)console, 0, 0, 0));
-        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)hook, 0, 0, 0));
-
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)console, hModule, 0, 0));
+        //CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)hook, 0, 0, 0));
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
